53b9c6e3e6003d53eabc73e78aa490bdabbf528f
gokulvigneesh <sgvigneesh2002@gmail.com>
2023-05-30 19:45:39 +0530

    router commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./task";
+import NavBar1 from "./component1/tailwind.jsx";
+import Website from "./component2/task1";
+import Form from "./component3/form"
+import TodoList from "./component4/TodoList"
+import store from './component4/store';
+import { Provider } from 'react-redux';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/component1/tailwind" element={<NavBar1 />} />
+        <Route path="/component2/task1" element={<Website />} />
+        <Route path="/component3/form" element={<Form />} />
+        <Route path="/component4/TodoList" element={<Provider store={store}>
+      <TodoList />
+    </Provider>} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
